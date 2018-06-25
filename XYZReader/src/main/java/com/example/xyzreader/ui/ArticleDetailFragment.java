a21df9a0a05fcf8cded88262c330c53200a2896d
John Boutros <john.g.boutros@gmail.com>
2018-06-26 01:28:41 +0200

    DetailFragment updated: dynamic color toolbar + photo scrim

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -11,6 +11,7 @@ import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.graphics.Palette;
 import android.text.Html;
@@ -241,8 +242,13 @@ public class ArticleDetailFragment extends Fragment implements
                                 Palette p = Palette.generate(bitmap, 12);
                                 mMutedColor = p.getDarkMutedColor(0xFF333333);
                                 mPhotoView.setImageBitmap(imageContainer.getBitmap());
-                                mRootView.findViewById(R.id.meta_bar)
-                                        .setBackgroundColor(mMutedColor);
+                                CollapsingToolbarLayout collapsingToolbar
+                                        = mRootView.findViewById(R.id.detail_collapsing);
+                                collapsingToolbar.setBackgroundColor(mMutedColor);
+                                collapsingToolbar.setContentScrimColor(mMutedColor);
+                                collapsingToolbar.setStatusBarScrimColor(mMutedColor);
+//                                mRootView.findViewById(R.id.meta_bar)
+//                                        .setBackgroundColor(mMutedColor);
                                 updateStatusBar();
                             }
                         }
